4aaba4dde84e8610d1a99f1b1d497a9a5097e327
akhant <a.khantelev@mail.ru>
2023-12-10 15:41:11 +0700

    remove unnecessary files

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,31 +1,22 @@
 <script>
-	import Counter from './Counter.svelte';
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcome_fallback from '$lib/images/svelte-welcome.png';
+	import { createBackgroundAnimation } from '../functions/createBackgroundAnimation'
+	import { onMount } from 'svelte';
+	onMount(()=> {
+		createBackgroundAnimation()
+	})
 </script>
 
 <svelte:head>
 	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
+	<meta name="description" content="Anton Khantelev website" />
 </svelte:head>
 
-<section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcome_fallback} alt="Welcome" />
-			</picture>
-		</span>
-
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
-
-	<Counter />
+<section class="wrapper">
+	<div class="blur" />
+	<div class="blur" />
+	<div class="blur" />
+	<h1>Anton Khantelev</h1>
+	<h2>Front-end developer</h2>
 </section>
 
 <style>
@@ -37,23 +28,76 @@
 		flex: 0.6;
 	}
 
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		display: block;
+	h1,h2 {
+		color: var(--color-text-dark);
+		text-transform: uppercase;
+		margin: 2rem 0;
+		margin: 0 auto;
+		text-align: center;
+		z-index: 2;
 		position: relative;
 		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
 	}
 
-	.welcome img {
+	h1	{
+		font-size: 6vw;
+		font-weight: 800;
+	}
+
+	h2	{
+		font-size: 2vw;
+	}
+
+	:root {
+		--color-bg: #ecd5b8;
+		--blur-1-size: clamp(400px, 60vw, 60vw);
+		--blur-2-size: clamp(300px, 40vw, 40vw);
+		--blur-3-size: clamp(100px, 10vw, 10vw);
+		--blur-amount: clamp(40px, 5vw, 90px);
+	}
+
+	.blur {
 		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
+		z-index: 1;
+		border-radius: 900px;
+		filter: blur(var(--blur-amount));
+		background: var(--color-bg);
+	}
+
+	.blur:nth-child(1) {
+		height: var(--blur-1-size);
+		width: calc(var(--blur-1-size) * 1.1);
+	}
+
+	.blur:nth-child(2) {
+		filter: blur(var(--blur-amount)) hue-rotate(135deg) brightness(1.3);
+		height: var(--blur-2-size);
+		width: calc(var(--blur-2-size) * 0.75);
+		top: 20vh;
+		right: 20vh;
+	}
+
+	.blur:nth-child(3) {
+		filter: blur(var(--blur-amount)) hue-rotate(-135deg);
+		height: var(--blur-3-size);
+		width: calc(var(--blur-3-size) * 2);
+		bottom: 20vh;
+		left: 20vw;
+	}
+
+	section {
+		padding: 5vw;
+		height: 100vh;
+		background: #f4f4f4;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		flex-direction: column;
+		position: relative;
+		overflow: hidden;
+	}
+
+	* {
+		box-sizing: border-box;
 	}
 </style>
