4dd4c0b37cc826b35aa4494b30f8b3b7056715e9
akhant <a.khantelev@mail.ru>
2023-12-10 15:46:18 +0700

    add basic header

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,79 +1,50 @@
 <script>
 	import { page } from '$app/stores';
-	import logo from '$lib/images/svelte-logo.svg';
-	import github from '$lib/images/github.svg';
+
 </script>
 
 <header>
-	<div class="corner">
-		<a href="https://kit.svelte.dev">
-			<img src={logo} alt="SvelteKit" />
-		</a>
-	</div>
-
 	<nav>
-		<svg viewBox="0 0 2 3" aria-hidden="true">
-			<path d="M0,0 L1,2 C1.5,3 1.5,3 2,3 L2,0 Z" />
-		</svg>
 		<ul>
 			<li aria-current={$page.url.pathname === '/' ? 'page' : undefined}>
 				<a href="/">Home</a>
 			</li>
-			<li aria-current={$page.url.pathname === '/about' ? 'page' : undefined}>
-				<a href="/about">About</a>
+			<li aria-current={$page.url.pathname === '/aboutme' ? 'page' : undefined}>
+				<a href="/aboutme">about me</a>
+			</li>
+			<li aria-current={$page.url.pathname === '/skills' ? 'page' : undefined}>
+				<a href="/skills">skills</a>
+			</li>
+			<li aria-current={$page.url.pathname === '/experience' ? 'page' : undefined}>
+				<a href="/experience">experience</a>
+			</li>
+			<li aria-current={$page.url.pathname === '/contacts' ? 'page' : undefined}>
+				<a href="/contacts">contacts</a>
 			</li>
 		</ul>
-		<svg viewBox="0 0 2 3" aria-hidden="true">
-			<path d="M0,0 L0,3 C0.5,3 0.5,3 1,2 L2,0 Z" />
-		</svg>
 	</nav>
-
-	<div class="corner">
-		<a href="https://github.com/sveltejs/kit">
-			<img src={github} alt="GitHub" />
-		</a>
-	</div>
+	
 </header>
 
 <style>
 	header {
 		display: flex;
 		justify-content: space-between;
-	}
-
-	.corner {
-		width: 3em;
-		height: 3em;
-	}
-
-	.corner a {
-		display: flex;
-		align-items: center;
-		justify-content: center;
+		position: fixed;
+		top: 0;
+		height: var(--header-height);
 		width: 100%;
-		height: 100%;
-	}
-
-	.corner img {
-		width: 2em;
-		height: 2em;
-		object-fit: contain;
+		z-index: 2;
 	}
 
 	nav {
 		display: flex;
 		justify-content: center;
-		--background: rgba(255, 255, 255, 0.7);
-	}
-
-	svg {
-		width: 2em;
-		height: 3em;
-		display: block;
-	}
-
-	path {
-		fill: var(--background);
+		position: absolute;
+		top: 0;
+		left: 50%;
+		transform: translateX(-50%);
+		color: black;
 	}
 
 	ul {
@@ -94,33 +65,44 @@
 		height: 100%;
 	}
 
-	li[aria-current='page']::before {
-		--size: 6px;
-		content: '';
-		width: 0;
-		height: 0;
-		position: absolute;
-		top: 0;
-		left: calc(50% - var(--size));
-		border: var(--size) solid transparent;
-		border-top: var(--size) solid var(--color-theme-1);
-	}
-
 	nav a {
 		display: flex;
 		height: 100%;
 		align-items: center;
 		padding: 0 0.5rem;
-		color: var(--color-text);
+		color: var(--color-text-dark);
 		font-weight: 700;
 		font-size: 0.8rem;
 		text-transform: uppercase;
 		letter-spacing: 0.1em;
 		text-decoration: none;
 		transition: color 0.2s linear;
+		white-space: nowrap;
 	}
 
 	a:hover {
 		color: var(--color-theme-1);
+	}
+
+	@media screen and (max-width: 430px) {
+		nav a {
+			font-size: 0.7rem;
+			font-weight: normal;
+		}
+	}
+
+	
+	@media screen and (max-width: 380px) {
+		nav a {
+			font-size: 0.6rem;
+			font-weight: normal;
+		}
+	}
+
+	@media screen and (max-width: 340px) {
+		nav a {
+			font-size: 0.5rem;
+			font-weight: normal;
+		}
 	}
 </style>
